0b8673ad4f0240e63617c18964ec7779e87a0235
Alexandre Barbosa <xnd.dev@gmail.com>
2025-01-22 20:02:43 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+import { QuizContext } from './context/quiz';
+
+import Welcome from './components/welcome';
+import Question from './components/Question';
+
+import './App.css'
+
+
+function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  return (
+    <div className="App">
+      <h1>Quiz de Programação</h1>    
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Playing" && <Question />}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Question.jsx
@@ -0,0 +1,21 @@
+import { useContext } from 'react';
+import { QuizContext } from '../context/quiz';
+
+function Question() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  console.log(quizState);
+
+  return (
+    <div id="question">
+      <p>Pergunta {quizState.currentQuestion + 1} de {quizState.questions.length} </p>
+      <h2>Pergunta atual</h2>
+      <div id="options-container">
+        <p>Opções</p>
+      </div>
+      <button>Continuar</button>
+    </div>
+  )
+}
+
+export default Question
