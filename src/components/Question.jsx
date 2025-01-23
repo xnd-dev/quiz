d32ee734bc63fe758a83e9b95d3d9c64974d0419
Alexandre Barbosa <xnd.dev@gmail.com>
2025-01-22 22:51:25 -0300

    last commit

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,19 +1,41 @@
 import { useContext } from 'react';
 import { QuizContext } from '../context/quiz';
 
+import Option from './Option';
+
+import "./Question.css";
+
+
 function Question() {
   const [quizState, dispatch] = useContext(QuizContext);
+  const currentQuestion = quizState.questions[quizState.currentQuestion]
 
-  console.log(quizState);
+  const onSelectOption = (option) => {
+    dispatch({
+      type: "CHECK_ANSWER",
+      payload: {answer: currentQuestion.answer, option}
+    });
+  };
 
   return (
     <div id="question">
       <p>Pergunta {quizState.currentQuestion + 1} de {quizState.questions.length} </p>
-      <h2>Pergunta atual</h2>
+      <h2>{currentQuestion.question} </h2>
       <div id="options-container">
-        <p>Opções</p>
+        {currentQuestion.options.map((option) => (
+            <Option 
+              option={option} 
+              key={option} 
+              answer={currentQuestion.answer}
+              selectOption={() => onSelectOption(option)}
+            />
+        ))}
       </div>
-      <button>Continuar</button>
+      {quizState.answerSelected && (
+        <button onClick= {() => dispatch({ type: "CHANGE_QUESTION" })}>
+          Continuar
+        </button>
+      )}
     </div>
   )
 }
--- src/context/quiz.jsx
+++ src/context/quiz.jsx
@@ -7,20 +7,65 @@ const initialState = {
     gameStage: STAGES[0],
     questions,
     currentQuestion: 0,
+    score: 0,
+    answerSelected: false,
 }
 
 const quizReducer = (state, action) => {
-    console.log(state, action);
 
     switch(action.type) {
         case "CHANGE_STATE":
-            return {
+          return {
+              ...state,
+              gameStage: STAGES[1],
+          };
+
+        case "REORDER_QUESTIONS":
+          const reorderedQuestions = questions.sort(() => {
+            return Math.random() - 0.5;
+          });
+
+          return {
+            ...state,
+            questions: reorderedQuestions,
+          };
+
+        case "CHANGE_QUESTION":
+          const nextQuestion = state.currentQuestion + 1;
+          let endGame = false;
+
+          if(!questions[nextQuestion]){
+                endGame = true;
+          }
+
+          return{
                 ...state,
-                gameStage: STAGES[1],
-            };
+                currentQuestion: nextQuestion,
+                gameStage: endGame ? STAGES[2] : state.gameStage,
+                answerSelected: false,
+          };
+
+        case "NEW_GAME":
+          return initialState;
+
+        case "CHECK_ANSWER":
+          if(state.answerSelected) return state;
+
+          const answer = action.payload.answer;
+          const option = action.payload.option;
+          let correctAnswer = 0;
+
+          if(answer === option) correctAnswer = 1;
+
+          return {
+            ...state,
+            score: state.score + correctAnswer,
+            answerSelected: option,
+          }
+
 
         default:
-            return state; 
+          return state; 
     }
 };
 
